a916342f09845fee9bb7605de3161ca14cb55ca3
Andreas Tennert <mail@andreas-tennert.de>
2020-10-10 19:19:29 +0200

    feat: add listing of categories

--- src/lcarsde-application-starter.py
+++ src/lcarsde-application-starter.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+
 import gi
+import os
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import GdkX11, Gdk, Gtk, GLib
+from gi.repository import Gtk
 
 
 css = b'''
@@ -31,6 +33,9 @@ css = b'''
     padding: 0;
     margin: 0;
 }
+.category {
+    color: #f90;
+}
 .spacer {
     background-color: #99C;
     outline-style: none;
@@ -46,10 +51,17 @@ css = b'''
 
 class LcarsdeApplicationStarter(Gtk.Window):
     """
-        Application menu main window
+    Application selector main window
     """
+    PREFERRED_CATEGORIES = ["System", "Game", "Network", "Office", "Settings", "AudioVideo", "Development", "Graphics",
+                            "Utility"]
+
     def __init__(self):
         Gtk.Window.__init__(self, title="Application Selector")
+
+        self.applications = {}
+        self.load_system_applications()
+        self.load_user_applications()
 
         self.css_provider = Gtk.CssProvider()
         self.css_provider.load_from_data(css)
@@ -58,6 +70,7 @@ class LcarsdeApplicationStarter(Gtk.Window):
         scroll_container.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
 
         self.app_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
+        self.display_apps()
 
         scroll_container.add(self.app_container)
         self.add(scroll_container)
@@ -65,13 +78,73 @@ class LcarsdeApplicationStarter(Gtk.Window):
         self.get_style_context().add_class("window")
         self.get_style_context().add_provider(self.css_provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
 
-# TODO read system apps
-# TODO read user apps
-# TODO make list of categories
-# TODO sort apps into categories
-# TODO display categories
-# TODO show corresponding apps as buttons under categories
-# TODO run corresponding app when clicking a button
+    def add_application(self, app_data):
+        data_lines = app_data.splitlines()
+        name = None
+        categories = None
+        exe = None
+        for line in data_lines:
+            if line.startswith("Name="):
+                name = line[5:]
+            elif line.startswith("Categories="):
+                categories = line[11:]
+            elif line.startswith("Exec="):
+                exe = line[5:]
+
+            if name is not None and categories is not None and exe is not None:
+                break
+
+        if not categories:
+            categories = "Utility"
+
+        categories = list(categories.split(";"))
+        category = self.get_category(categories).strip()
+        if category == "":
+            category = "Utility"
+
+        if category not in self.applications.keys():
+            self.applications[category] = set()
+
+        self.applications[category].add((name, exe))
+
+    def get_category(self, categories):
+        for preferred_category in self.PREFERRED_CATEGORIES:
+            if preferred_category in categories:
+                return preferred_category
+        return categories[0]
+
+    def load_applications(self, app_directory):
+        app_files = [os.path.join(app_directory, f)
+                     for f in os.listdir(app_directory)
+                     if os.path.isfile(os.path.join(app_directory, f))]
+        app_data = set()
+        for file_path in app_files:
+            try:
+                with open(file_path, 'r') as file:
+                    app_data.add(file.read())
+            except FileNotFoundError:
+                print("Unable to load {0}".format(file_path))
+
+        for d in app_data:
+            self.add_application(d)
+
+    def load_system_applications(self):
+        self.load_applications("/usr/share/applications")
+
+    def load_user_applications(self):
+        path = "{0}/.local/share/applications".format(os.environ.get('HOME'))
+        self.load_applications(path)
+
+    def display_apps(self):
+        for category, apps in self.applications.items():
+            category_label = Gtk.Label(label=category)
+            category_label.get_style_context().add_class("category")
+            category_label.get_style_context().add_provider(self.css_provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
+            self.app_container.add(category_label)
+
+    @staticmethod
+    def start_application(widget):
+        pass
 
 
 if __name__ == "__main__":
